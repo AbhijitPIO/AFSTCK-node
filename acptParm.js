4e67952594876edbeba142ecc3f87d9bb858095f
AbhijitPIO <abhijit.fulambrikar@programmers.io>
2022-06-27 20:08:03 +0530

    1 file modified

--- acptParm.js
+++ acptParm.js
@@ -1,11 +1,18 @@
-const command = process.argv[2]
+const command = process.argv[2];
+//const yargs = require('yargs')
 
-console.log(process.argv)
+console.log(process.argv);
+//console.log(yargs.argv)
+// customize yargs version if required
+// yargs.version('1.1.0')
+// Create Add command
+// yargs.command({command: 'add',
+//describe: 'Add a new note',
+//handler: function() {console.log('Adding a new note')}}
+//)
 
-if (command === 'add')
-{
-  console.log('Adding Note!')
-} else if (command === 'remove')
-{
-console.log('Removing Note!')
+if (command === 'add') {
+  console.log('Adding Note!');
+} else if (command === 'remove') {
+  console.log('Removing Note!');
 }
